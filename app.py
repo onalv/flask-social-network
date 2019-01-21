ecac7b8e3d81ac68ed804b9814bf222c7074e5f8
Omar <omarnavaalvarez@gmail.com>
2019-01-20 21:23:32 -0500

    started LoginManager logic

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, g
+from flask_login import LoginManager()
 
 import models
 
@@ -7,12 +8,24 @@ PORT = 8000
 HOST = '0.0.0.0'
 
 app = Flask(__name__)
+app.secret_key = 'sd123.-/sd13¡?*asd123asd46qwegdsnmdmh3487ryduiskwjh'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view('login')
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 @app.before_request
 def before_request():
     """Connect to the db before any request"""
     if not hasattr(g, 'db'):
-        g.db = models.DATABASE
+        g.db = models.DB
         g.db.connect()
 
 @app.after_request
