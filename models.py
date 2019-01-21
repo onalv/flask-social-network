5db7629aac27846aa7e8b27032af4b0b3b954e37
Omar <omarnavaalvarez@gmail.com>
2019-01-20 21:10:48 -0500

    create methods and class for User

---
+++ models.py
@@ -0,0 +1,29 @@
+import datetime
+
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
+
+from peewee import *
+
+DATABASE = SqliteDatabase('social.db')
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=120)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-joined-at')
+
+    @classmethod
+    def create_user(cls, username, email, password):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+            )
+        except IntegrityError:
+            raise ValueError('User already exists')
